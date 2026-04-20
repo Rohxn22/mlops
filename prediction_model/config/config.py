b69a6be571ac7d39b3295be3c0bdfea0c97823de
Rohan <rohanrc224@gmail.com>
2026-04-20 08:30:34 +0530

    clean & updated

--- prediction_model/config/config.py
+++ prediction_model/config/config.py
@@ -1,60 +1,35 @@
-import pathlib
 import os
 
+current_directory = os.path.dirname(os.path.realpath(__file__))
+PACKAGE_ROOT = os.path.dirname(current_directory)
 
-current_directory = os.path.dirname(os.path.realpath(__file__)) #current directory of the script
-
-PACKAGE_ROOT = os.path.dirname(current_directory) #parent directory of current directory
-
-
-# PACKAGE_ROOT = pathlib.Path(prediction_model.__file__).resolve().parent
-
-DATAPATH = os.path.join(PACKAGE_ROOT,"datasets")
+DATAPATH = os.path.join(PACKAGE_ROOT, "datasets")
 
 TRAIN_FILE = 'train.csv'
-TEST_FILE = 'test.csv'
+TEST_FILE  = 'test.csv'
 
 TARGET = 'Loan_Status'
 
-#Final features used in the model
 FEATURES = ['Gender', 'Married', 'Dependents', 'Education',
-       'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
-       'Loan_Amount_Term', 'Credit_History', 'Property_Area']
+            'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome',
+            'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area']
 
 NUM_FEATURES = ['ApplicantIncome', 'LoanAmount', 'Loan_Amount_Term']
 
-CAT_FEATURES = ['Gender',
- 'Married',
- 'Dependents',
- 'Education',
- 'Self_Employed',
- 'Credit_History',
- 'Property_Area']
+CAT_FEATURES = ['Gender', 'Married', 'Dependents', 'Education',
+                'Self_Employed', 'Credit_History', 'Property_Area']
 
-# in our case it is same as Categorical features
-FEATURES_TO_ENCODE = ['Gender',
- 'Married',
- 'Dependents',
- 'Education',
- 'Self_Employed',
- 'Credit_History',
- 'Property_Area']
+FEATURES_TO_ENCODE = ['Gender', 'Married', 'Dependents', 'Education',
+                      'Self_Employed', 'Credit_History', 'Property_Area']
 
 FEATURE_TO_MODIFY = ['ApplicantIncome']
-FEATURE_TO_ADD = 'CoapplicantIncome'
+FEATURE_TO_ADD    = 'CoapplicantIncome'
+DROP_FEATURES     = ['CoapplicantIncome']
+LOG_FEATURES      = ['ApplicantIncome', 'LoanAmount']
 
-DROP_FEATURES = ['CoapplicantIncome']
+S3_BUCKET    = "mlops-dataset-792633646256-eu-north-1-an"
+FOLDER       = "datadrift"
 
-LOG_FEATURES = ['ApplicantIncome', 'LoanAmount'] # taking log of numerical columns
-
-S3_BUCKET = "mlops-dataset-792633646256-eu-north-1-an"
-
-FOLDER="datadrift"
-
-TRACKING_URI="http://ec2-13-63-102-32.eu-north-1.compute.amazonaws.com:5000/"
-
-
-EXPERIMENT_NAME="loan_prediction_model"
-
-MODEL_NAME="/Loanprediction-model"
-
+TRACKING_URI    = "http://ec2-13-63-102-32.eu-north-1.compute.amazonaws.com:5000/"
+EXPERIMENT_NAME = "loan_prediction_model"
+MODEL_NAME      = "/Loanprediction-model"
--- prediction_model/processing/data_handling.py
+++ prediction_model/processing/data_handling.py
@@ -2,9 +2,7 @@ import os
 import pandas as pd
 from prediction_model.config import config
 
-#Load the dataset
-def load_dataset(file_name):
-    filepath = os.path.join(config.DATAPATH,file_name)
-    _data = pd.read_csv(filepath)
-    return _data
 
+def load_dataset(file_name):
+    filepath = os.path.join(config.DATAPATH, file_name)
+    return pd.read_csv(filepath)

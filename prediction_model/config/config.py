b2dab15ff2b7a6cfff445ca12e34ecde7af0c440
Rohan <rohanrc224@gmail.com>
2026-04-23 10:12:29 +0530

    retrain on new dataset

--- prediction_model/config/config.py
+++ prediction_model/config/config.py
@@ -7,7 +7,7 @@ DATAPATH = os.path.join(PACKAGE_ROOT, "datasets")
 
 # Single source dataset — will be split 75/25 in data_handling
 # NOTE: Changing this will automatically update CI/CD and Docker build
-DATASET_FILE = 'loan_data_part_2.csv'
+DATASET_FILE = 'loan_data_part_3.csv'
 
 TARGET = 'loan_status'
 
@@ -30,8 +30,8 @@ FEATURES_TO_ENCODE = ['occupation_status', 'product_type', 'loan_intent']
 
 # MLflow — reuse same server, separate experiment
 TRACKING_URI    = "http://ec2-13-63-102-32.eu-north-1.compute.amazonaws.com:5000/"
-EXPERIMENT_NAME = "loan_prediction_v2"
-MODEL_NAME      = "Loanprediction-model"
+EXPERIMENT_NAME = "loan_prediction_v2_d3"
+MODEL_NAME      = "Loanprediction-model-d3"
 
 S3_BUCKET = "mlops-dataset-792633646256-eu-north-1-an"
 FOLDER    = "datadrift_v2"
